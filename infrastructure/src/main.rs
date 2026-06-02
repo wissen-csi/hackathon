e2f53ae6712435f38e29eefcdb0ca1fb4f29a025
Jorge <wissencsi@gmail.com>
2026-06-02 00:08:27 -0500

    add hexagonal architecture and workspace

--- infrastructure/src/main.rs
+++ infrastructure/src/main.rs
@@ -1,5 +1,5 @@
 #[tokio::main]
 async fn main() {
-    print!("I will start on 07/30/2026");
+    print!("I will start on 0/03/2026");
     print!("for now I want relax and learn more about rust")
 }
